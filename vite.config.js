f4051225777ea3836988b018b0da2f4cdd062fd3
Thisal Tennakoon <thisaltennakoon@gmail.com>
2025-06-24 14:11:11 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'ReactHelloLib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-hello-lib.${format}.js`
+    },
+    rollupOptions: {
+      // externalize deps
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
